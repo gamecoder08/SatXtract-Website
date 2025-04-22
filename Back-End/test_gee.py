4d7d3f407e79f55c6ef5f894725c2c2dcc65d50e
Debasish Ray <debasishray3275@gmail.com>
2025-04-22 18:47:40 +0530

    GEE_Polygon Code

--- Back-End/test_gee.py
+++ Back-End/test_gee.py
@@ -18,6 +18,8 @@ end_date = '2023-01-31'
 # Call the function
 result = fetch_map_uhi(latitude, longitude, zoom, start_date, end_date)
 
+# result = fetch_map_uhi_v2(polygon_coords,zoom , start_date, end_date)
+
 # Print the URLs returned
 print("GEE Map URLs:")
 for key, url in result.items():
